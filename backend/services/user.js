0ed4c2c4810fe403437bd0848c5cd6c9b1a15c97
arihant-ttn <arihant.kulshrestha@tothenew.com>
2025-05-02 11:54:02 +0530

    fixed User service

--- backend/services/user.js
+++ backend/services/user.js
@@ -1,22 +1,22 @@
 import pool from '../db/index.js';
 
 
-export const addClient = async(req,res)=>{
+export const addUser = async(req,res)=>{
 
     const {name,email,password_hash,role,profile_image}=req.body
     const values = [name,email,password_hash,role,profile_image]
 
     try{
-        const users = await pool.query("INSERT INTO projects(name,email,password_hash,role,profile_image) VALUES ($1, $2, $3, $4, $5)",values);
+        const users = await pool.query("INSERT INTO users(name,email,password_hash,role,profile_image) VALUES ($1, $2, $3, $4, $5)",values);
 
         return{
             status:200,
             success:true,
-            data:'Project Added'
+            data:'User Added'
         }
 
     }catch(error){
-        console.log("Error in Adding Project from backend", error);
+        console.log("Error in Adding Users from backend", error);
         return{
             status:500,
             success:false,
