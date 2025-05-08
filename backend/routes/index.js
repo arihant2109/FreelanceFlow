c07466ae2f54302c97f792995dd47c279cf50aad
arihant-ttn <arihant.kulshrestha@tothenew.com>
2025-05-08 15:14:47 +0530

    prompt parser route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,9 @@
 import { addClientController } from "../controllers/clientController.js";
 import { addProjectController } from "../controllers/projectController.js";
 import { addUserController } from "../controllers/userController.js";
+import promptparser from '../controllers/prompt.js';
+
+
 import express from 'express';
 
 const router = express.Router();
@@ -8,5 +11,5 @@ const router = express.Router();
 router.post('/addClient',addClientController);
 router.post('/addUser',addUserController);
 router.post('/addProject',addProjectController);
-
+router.use('/prompt',promptparser);
 export default router;  
